c02bca88c0d7beb603114f6db1c885072f7198a2
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-01-31 19:42:52 +0530

    Added the scripts for update the countdown timer...

--- script.js
+++ script.js
@@ -4,7 +4,24 @@ const hoursEl = document.getElementById("hours");
 const minutesEl = document.getElementById("minutes");
 const secondsEl = document.getElementById("seconds");
 
-const currentFullYear = new Date().getFullYear();
+const currentYear = new Date().getFullYear();
 
-const newYearTime = new Date(`January 01 ${currentFullYear + 1} 00:00:00`);
-console.log(newYearTime);
+const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
+
+function updateCountDown() {
+  const currentTime = new Date();
+
+  const diff = newYearTime - currentTime;
+
+  const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+  const h = Math.floor(diff / 1000 / 60 / 60) % 24;
+  const m = Math.floor(diff / 1000 / 60) % 60;
+  const s = Math.floor(diff / 1000) % 60;
+
+  daysEl.innerText = d < 10 ? "0" + d : d;
+  hoursEl.innerText = h < 10 ? "0" + h : h;
+  minutesEl.innerText = m < 10 ? "0" + m : m;
+  secondsEl.innerText = s < 10 ? "0" + s : s;
+}
+
+setInterval(updateCountDown, 1000);
